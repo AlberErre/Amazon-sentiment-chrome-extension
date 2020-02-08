ff39e1195ceb834e73a9f44a98b6fdcda24d01cd
Alber Erre <arblugo@gmail.com>
2020-02-08 20:42:49 +0000

    add amazon domains for background and content scripts

--- background/index.js
+++ background/index.js
@@ -4,6 +4,8 @@
 
 "use strict";
 
+// IMPORTANT: hostEquals must be explicit domain such as www.amazon.com, non-explicit domains do not work (*.amazon.com)
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({ color: "#3aa757" }, function() {
     console.log("The color is green.");
@@ -13,7 +15,16 @@ chrome.runtime.onInstalled.addListener(function() {
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "developer.chrome.com" }
+            pageUrl: { hostEquals: "www.amazon.com" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "www.amazon.co.uk" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "www.amazon.ca" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "www.amazon.com.au" }
           })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
